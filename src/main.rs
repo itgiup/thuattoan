2d53bd748102508dbdc37b4e7afc7673fe88420e
itgiup <itgiup.com@gmail.com>
2022-09-02 01:06:48 +0700

    new

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,27 @@ fn main() {
     let my_sort = sort::Sort {
         array: vec![5, 3, 1, 7, 3, 8, 6, 9, 2, 4],
     };
-    let bsort = my_sort.bubble_sort(false);
-    let bsort = my_sort.quick_sort(true);
-    print!("{:?}", &bsort);
+    let mut array = my_sort.get_array();
+
+    println!("quick sort:");
+    let qsort = my_sort.quick_sort(array, true);
+
+    let n: i32 = -10;
+    println!("bubble sort:");
+    let bsort = my_sort.bubble_sort(true);
+
+    println!(
+        "\n|\nV\n\n
+        array:
+        {:?}
+
+        quick sort:
+        {:?}
+
+        bubble sort:
+        {:?}",
+        my_sort.array, qsort, bsort
+    );
 
     println!("\n\n");
 }
--- src/sort.rs
+++ src/sort.rs
@@ -1,22 +1,93 @@
+// use rand::Rng;
+// #[derive(Copy, Clone)]
 pub struct Sort {
     pub array: Vec<i32>,
 }
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
 
 impl Sort {
-    pub fn copy(&self) -> Vec<i32> {
+    pub fn get_array(&self) -> Vec<i32> {
         let new_array = &self.array.clone();
 
         return new_array.to_vec();
     }
 
-    pub fn quick_sort(&self, print: bool) -> Vec<i32> {
-        let mut array = self.copy();
+    // /* quick_sort */
+    // pub fn partition(&self, mut array: Vec<i32>, low: i32, high: i32) -> i32 {
+    //     let pivot = low.clone() as usize;
+    //     let mut index = high.clone() as usize;
+    //     for i in low..high {
+    //         let ui = i as usize;
+    //         if array[ui] < array[pivot] {
+    //             (array[ui], array[index]) = (array[index], array[ui]);
+    //             index += 1;
+    //         }
+    //     }
+    //     (array[pivot], array[index]) = (array[pivot], array[index]);
+    //     return index as i32;
+    // }
+    // pub fn random_pivot_partition(&self, mut array: Vec<i32>, low: i32, high: i32) -> i32 {
+    //     let n = rand::thread_rng().gen_range(0..array.len()) as i32;
+    //     let pvt = (low + n % (high - low + 1)) as usize;
+    //     (array[high as usize], array[pvt]) = (array[pvt], array[high as usize]);
+    //     return self.partition(array, low, high);
+    // }
+    // fn _quick_sort(&self, array: Vec<i32>, low: i32, high: i32) -> i32 {
+    //     let pindex: i32;
+    //     if low < high {
+    //         pindex = self.random_pivot_partition(array, low, high);
+    //         self._quick_sort(array, low, pindex - 1);
+    //         self._quick_sort(array, pindex + 1, high);
+    //     }
+    //     return 0;
+    // }
+    // pub fn quick_sort(&self, print: bool) -> Vec<i32> {
+    //     let array = self.get_array();
+    //     println!("{:p} {:?}", &array[0], array);
+    //     let len = array.len();
+    //     // let pivot_i = rand::thread_rng().gen_range(0..len);
+    //     self._quick_sort(array, 0, (len - 1) as i32);
+    //     return Vec::new();
+    // }
 
-        return array;
+    /* quick_sort */
+    pub fn quick_sort(&self, array: Vec<i32>, print: bool) -> Vec<i32> {
+        if array.len() <= 1 {
+            return array;
+        }
+        let pivot = array[array.len() - 1];
+
+        let mut array_less: Vec<i32> = Vec::new();
+        let mut array_greater: Vec<i32> = Vec::new();
+        let mut array_sorted: Vec<i32> = Vec::new();
+        for i in 0..(array.len() - 1) {
+            if array[i] > pivot {
+                array_greater.push(array[i]);
+            } else {
+                array_less.push(array[i]);
+            }
+        }
+        println!("{:?}      {:}      {:?}", array_less, pivot, array_greater);
+        // quick_sort(array_less);
+        array_sorted.append(&mut self.quick_sort(array_less, print));
+        array_sorted.push(pivot);
+        array_sorted.append(&mut self.quick_sort(array_greater, print));
+
+        // if print {
+        //     println!(
+        //         "{:} {:?}",
+        //         pivot,
+        //         array_sorted //, &array_greater, &array_less
+        //     );
+        // }
+        return array_sorted;
     }
 
+    /* bubble_sort */
     pub fn bubble_sort(&self, print: bool) -> Vec<i32> {
-        let mut array = self.copy();
+        let mut array = self.get_array();
 
         for i in 0..array.len() {
             let i_ = array.len() - i - 1;
